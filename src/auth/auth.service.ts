817fc5c8c810592cf898a7d588691d1e4903df38
Jeong seulho <wjdtmfgh@gmail.com>
2024-08-04 23:13:11 +0900

    Feat : SignIn

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -10,7 +11,17 @@ export class AuthService {
     private userRepository: UserRepository,
   ) {}
 
-  async signUp(AuthCredentialsDto: AuthCredentialsDto): Promise<void> {
-    return this.userRepository.createUser(AuthCredentialsDto);
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.userRepository.findOne({ where: { username } });
+
+    if (user && (await bcrypt.compare(password, user.password)))
+      return 'Login Success';
+
+    throw new UnauthorizedException('Login failed');
   }
 }
